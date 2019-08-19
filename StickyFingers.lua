e2f1c900747366713d204abc8d38a9808110f28d
NewbieC4 <hellvector@gmail.com>
2019-08-19 18:36:34 -0500

    Version 0.6.1
    Added a slash command for players to see their session and lifetime 
    pickpocketing totals.

--- StickyFingers.lua
+++ StickyFingers.lua
@@ -1,16 +1,20 @@
--- Version 0.1    Prints all combat actions to chat.
--- Version 0.2    Only looks at successful casts made by the player.
--- Version 0.21   Also checks to see if the spell cast was Pick Pocket.
--- Version 0.3    Activate a switch when the player picks a pocket.
--- Version 0.31   Picked pocked switch is turned off when the player loots money
---                or changes targets.
--- Version 0.4    Start tracking the money looted from pickpocketing.
--- Version 0.41   Session and lifetime totals are now calculated.
--- Version 0.42   Added formatting for the money amounts when they are printed.
--- Version 0.5    Add a check to see if the player is a Rogue, and unregister events if not.
+-- Version 0.1      Prints all combat actions to chat.
+-- Version 0.2      Only looks at successful casts made by the player.
+-- Version 0.21     Also checks to see if the spell cast was Pick Pocket.
+-- Version 0.3      Activate a switch when the player picks a pocket.
+-- Version 0.31     Picked pocked switch is turned off when the player loots money
+--                  or changes targets.
+-- Version 0.4      Start tracking the money looted from pickpocketing.
+-- Version 0.41     Session and lifetime totals are now calculated.
+-- Version 0.42     Added formatting for the money amounts when they are printed.
+-- Version 0.5      Added a check to see if the player is a Rogue, and unregister events if not.
+-- Version 0.6      Added a slash command for interracting with the addon.
+-- Version 0.6.1    Commented out the validation print commands throughout. All results moved to the slash command.
 
 -- Save the player GUID for spell checking.
 local playerGUID = UnitGUID("player")
+-- Save the player class for addon activation.
+local playerClass, playerClassName = UnitClass("player")
 -- Picked pocket check switch.
 local picked = false
 -- Player money used for reference.
@@ -23,10 +27,14 @@ local function eventHandler(self, event, ...)
 
   if event == "PLAYER_LOGIN" then
     -- Initialization message to verify the addon loaded.
-    DEFAULT_CHAT_FRAME:AddMessage("Sticky fingers leave empty pockets.")
+    if playerClassName == "ROGUE" then
+      DEFAULT_CHAT_FRAME:AddMessage("Sticky fingers leave empty pockets.")
+    else
+      DEFAULT_CHAT_FRAME:AddMessage("Silly rabbit, StickyFingers is for Rogues!")
+    end
   end
 
-  if (event == "ADDON_LOADED" and StickyFingersLoot == nil) then
+  if ((event == "ADDON_LOADED" and playerClassName == "ROGUE") and StickyFingersLoot == nil) then
     -- If no saved lifetime loot value exists, initialize at 0.
     StickyFingersLoot = 0
   end
@@ -41,17 +49,17 @@ local function eventHandler(self, event, ...)
       local spellId, spellName, spellSchool = select(12, CombatLogGetCurrentEventInfo())
 
       -- Verification message to confirm that a spell cast was registered.
-      print("You did "..spellName.." to "..destName.."!")
+      --print("You did "..spellName.." to "..destName.."!")
 
       -- Verify that the spell which was cast is Pick Pocket.
       if spellId == 921 then
         -- Check how much money the player has before looting.
         playerMoney = GetMoney()
-        print("Player has "..GetCoinText(playerMoney,", ")..".")
-        print("What has he got in HIS pocketses?")
+        --print("Player has "..GetCoinText(playerMoney,", ")..".")
+        --print("What has he got in HIS pocketses?")
         -- Set picked pocket switch to ON.
         picked = true
-        print("Picked is true.")
+        --print("Picked is true.")
       end
 
     end
@@ -69,14 +77,12 @@ local function eventHandler(self, event, ...)
     StickyFingersLoot = (StickyFingersLoot + pocketchange)
     -- Set picked pocket switch to OFF.
     picked = false
-    print("Money has changed hands...")
-    print("Player looted "..GetCoinTextureString(pocketchange).."!")
-    print("Player now has "..GetCoinText(newMoney,", ")..".")
-    print("Player has picked "..GetCoinText(pickedMoney,", ").." this session!")
-    print("Player has picked "..GetCoinText(StickyFingersLoot,", ").." in their life!")
-    -- Below is an over complicated way to get similar results, and it may be necessary if GetCoinText doesn't work in Classic.
-    --print(("Player has picket %d Gold, %d Silver, %d Copper in their life!"):format(StickyFingersLoot / 100 / 100, (StickyFingersLoot / 100) % 100, StickyFingersLoot % 100))
-    print("Picked is false.")
+    --print("Money has changed hands...")
+    --print("Player looted "..GetCoinTextureString(pocketchange).."!")
+    --print("Player now has "..GetCoinText(newMoney,", ")..".")
+    --print("Player has picked "..GetCoinText(pickedMoney,", ").." this session!")
+    --print("Player has picked "..GetCoinText(StickyFingersLoot,", ").." in their life!")
+    --print("Picked is false.")
   end
 
   -- If the player changes targets or enters combat while the picked pocket switch is ON.
@@ -84,16 +90,33 @@ local function eventHandler(self, event, ...)
     -- Thanks to Jinst!
     -- Set picked pocket switch to OFF.
     picked = false
-    print("Picked is false.")
+    --print("Picked is false.")
   end
 
 end
 
+-- Slash command handler for displaying the player's statistics.
+function StickyFingersSlashCommand(msg)
+  if pickedMoney > 0 then
+    print("Player has picked "..GetCoinText(pickedMoney,", ").." this session!")
+  end
+  print("Player has picked "..GetCoinText(StickyFingersLoot,", ").." in their life!")
+  -- Below is an over complicated way to get similar results, and it may be necessary if GetCoinText doesn't work in Classic.
+  --print(("Player has picket %d Gold, %d Silver, %d Copper in their life!"):format(StickyFingersLoot / 100 / 100, (StickyFingersLoot / 100) % 100, StickyFingersLoot % 100))
+end
+
+
 local StickyFingers = CreateFrame("Frame")
 StickyFingers:RegisterEvent("PLAYER_LOGIN")
 StickyFingers:RegisterEvent("ADDON_LOADED")
-StickyFingers:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-StickyFingers:RegisterEvent("PLAYER_MONEY")
-StickyFingers:RegisterEvent("PLAYER_TARGET_CHANGED")
-StickyFingers:RegisterEvent("PLAYER_REGEN_DISABLED")
+if playerClassName == "ROGUE" then
+  StickyFingers:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+  StickyFingers:RegisterEvent("PLAYER_MONEY")
+  StickyFingers:RegisterEvent("PLAYER_TARGET_CHANGED")
+  StickyFingers:RegisterEvent("PLAYER_REGEN_DISABLED")
+end
 StickyFingers:SetScript("OnEvent", eventHandler)
+
+-- Establish the accepted slash commands.
+SLASH_STICKYFINGERS1, SLASH_STICKYFINGERS2, SLASH_STICKYFINGERS3 = "/stickyfingers", "/sticky", "/sf"
+SlashCmdList["STICKYFINGERS"] = StickyFingersSlashCommand
